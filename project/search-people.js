1c0f2e3ed2ee403911dc6991aa3c48b443e1447c
Alex-makaroff <root@amak.site>
2021-01-27 19:09:36 +0300

    first theme update

--- project/search-people.js
+++ project/search-people.js
@@ -81,10 +81,15 @@ function searchPeopleByMaxAge (arrPeople, parameters) {
 
 function getPeopleDiv (person) {
     return `<div class="people"> 
+<div ">
 <span class="img-name"><img  src="name.png" alt=""></span> 
-<span class="value">${person.name}</span> 
+<span class="value">${person.name}</span>
+</div> 
+<div>
 <span class="img-age"><img src="age.png" alt=""></span> 
 <span class="value">${person.age}</span>
+</div>
+
 </div>`;
 }
 
@@ -182,4 +187,102 @@ function reset () {
     document.getElementById('pattern').value = '';
     document.getElementById('minAge').value = '';
     document.getElementById('maxAge').value = '';
+}
+
+function day () {
+    document.body.style.backgroundColor = 'white';
+}
+function night () {
+    document.body.style.backgroundColor = '#3f3f47';
+}
+
+function neonTheme () {
+    const elemenstBtn = document.getElementsByClassName('btn-help');
+    for (const ell of elemenstBtn) {
+        ell.classList.remove('usual-theme-btn', 'neonTheme-btn', 'strangeTheme-btn', 'simpleTheme-btn');
+        ell.classList.add('neonTheme-btn');
+    }
+    const elementBtnReset = document.getElementsByClassName('btn-reset-help');
+    for (const ell of elementBtnReset) {
+        ell.classList.remove('usual-theme-btn-reset', 'neonTheme-btn-reset', 'strangeTheme-btn-reset', 'simpleTheme-btn-reset');
+        ell.classList.add('neonTheme-btn-reset');
+    }
+    const elementTitle = document.getElementsByClassName('title-help');
+    for (const ell of elementTitle) {
+        ell.classList.remove('usual-theme-title', 'neonTheme-title', 'strangeTheme-title', 'simpleTheme-title');
+        ell.classList.add('neonTheme-title');
+    }
+    const elementPeople = document.getElementsByClassName('people');
+    for (const ell of elementPeople) {
+        ell.classList.remove('usual-theme-people-list', 'neonTheme-people-list', 'strangeTheme-people-list', 'simpleTheme-people-list');
+        ell.classList.add('neonTheme-people-list');
+        night();
+    }
+}
+
+function usualTheme () {
+    const elemenstBtn = document.getElementsByClassName('btn-help');
+    for (const ell of elemenstBtn) {
+        ell.classList.remove('usual-theme-btn', 'neonTheme-btn', 'strangeTheme-btn', 'simpleTheme-btn');
+        ell.classList.add('usualTheme-btn');
+    }
+    const elementBtnReset = document.getElementsByClassName('btn-reset-help');
+    for (const ell of elementBtnReset) {
+        ell.classList.remove('usual-theme-btn-reset', 'neonTheme-btn-reset', 'strangeTheme-btn-reset', 'simpleTheme-btn-reset');
+        ell.classList.add('usualTheme-btn-reset');
+    }
+    const elementTitle = document.getElementsByClassName('title-help');
+    for (const ell of elementTitle) {
+        ell.classList.remove('usual-theme-title', 'neonTheme-title', 'strangeTheme-title', 'simpleTheme-title');
+        ell.classList.add('usualTheme-title');
+    }
+    const elementPeople = document.getElementsByClassName('people');
+    for (const ell of elementPeople) {
+        ell.classList.remove('usual-theme-people-list', 'neonTheme-people-list', 'strangeTheme-people-list', 'simpleTheme-people-list');
+        ell.classList.add('usualTheme-people-list');
+    }
+}
+function strangeTheme () {
+    const elemenstBtn = document.getElementsByClassName('btn-help');
+    for (const ell of elemenstBtn) {
+        ell.classList.remove('usual-theme-btn', 'neonTheme-btn', 'strangeTheme-btn', 'simpleTheme-btn');
+        ell.classList.add('strangeTheme-btn');
+    }
+    const elementBtnReset = document.getElementsByClassName('btn-reset-help');
+    for (const ell of elementBtnReset) {
+        ell.classList.remove('usual-theme-btn-reset', 'neonTheme-btn-reset', 'strangeTheme-btn-reset', 'simpleTheme-btn-reset');
+        ell.classList.add('strangeTheme-btn-reset');
+    }//
+    const elementTitle = document.getElementsByClassName('title-help');
+    for (const ell of elementTitle) {
+        ell.classList.remove('usual-theme-title', 'neonTheme-title', 'strangeTheme-title', 'simpleTheme-title');
+        ell.classList.add('strangeTheme-title');
+    }
+    const elementPeople = document.getElementsByClassName('people');
+    for (const ell of elementPeople) {
+        ell.classList.remove('usual-theme-people-list', 'neonTheme-people-list', 'strangeTheme-people-list', 'simpleTheme-people-list');
+        ell.classList.add('strangeTheme-people-list');
+    }
+}
+function simpleTheme () {
+    const elemenstBtn = document.getElementsByClassName('btn-help');
+    for (const ell of elemenstBtn) {
+        ell.classList.remove('usual-theme-btn', 'neonTheme-btn', 'strangeTheme-btn', 'simpleTheme-btn');
+        ell.classList.add('simpleTheme-btn');
+    }
+    const elementBtnReset = document.getElementsByClassName('btn-reset-help');
+    for (const ell of elementBtnReset) {
+        ell.classList.remove('usual-theme-btn-reset', 'neonTheme-btn-reset', 'strangeTheme-btn-reset', 'simpleTheme-btn-reset');
+        ell.classList.add('simpleTheme-btn-reset');
+    }//
+    const elementTitle = document.getElementsByClassName('title-help');
+    for (const ell of elementTitle) {
+        ell.classList.remove('usual-theme-title', 'neonTheme-title', 'strangeTheme-title', 'simpleTheme-title');
+        ell.classList.add('simpleTheme-title');
+    }
+    const elementPeople = document.getElementsByClassName('people');
+    for (const ell of elementPeople) {
+        ell.classList.remove('usual-theme-people-list', 'neonTheme-people-list', 'strangeTheme-people-list', 'simpleTheme-people-list');
+        ell.classList.add('simpleTheme-people-list');
+    }
 }
